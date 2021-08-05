dbc6e30a96612ab97fe05fd33ac4ee003ed7c15f
PawanSadhwani17 <84863380+PawanSadhwani17@users.noreply.github.com>
2021-08-05 11:20:17 +0530

    Update urls.py

--- the_guide/urls.py
+++ the_guide/urls.py
@@ -14,19 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from . import views
-from django.conf import settings
-from django.views.static import serve
-from django.conf.urls import url
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('guide', views.guide, name='guide'),
-    path('', include("Home.urls")),
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('guide', views.guide, name='guide')
 ]
